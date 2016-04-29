32badb5fc68fb9084a7951a3a2e04889e9c7122a
Steve Belovarich <steveblue@gmail.com>
2016-04-29 01:28:32 -0700

    Included boilerplate sass, preferred syntax in exmaple

--- app/js/app.ts
+++ app/js/app.ts
@@ -9,7 +9,7 @@ import { Component } from "angular2/core";
 `
 })
 class HelloWorld {
-  names: string[];
+  names: Array<string>;
   constructor() {
     this.names = ['Ari', 'Carlos', 'Felipe', 'Nate'];
   }
