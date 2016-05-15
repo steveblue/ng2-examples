529611e28739b1ef9a386cc6d9595b94d578a19c
Steve Belovarich <steveblue@gmail.com>
2016-05-15 02:51:46 -0700

    Sample multiple frequencies from audio waveform.

--- app/src/components/vizualizer.ts
+++ app/src/components/vizualizer.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ChangeDetectionStrategy, ChangeDetectorRef, ElementRef } from '@angular/core';
 import { MediaService } from '../services/media-service';
+import { Meter } from '../models/meter';
 
 declare let d3:any;
 
@@ -7,18 +8,48 @@ declare let d3:any;
 selector: 'visualizer',
 outputs: ['path'],
 template: `
+ <svg [attr.width]="44" 
+      [attr.height]="44"
+      (click)="addMeter($event)">
+   <line [attr.x1]="0" 
+         [attr.x2]="22" 
+         [attr.y1]="11" 
+         [attr.y2]="11"
+         [attr.stroke]="meters[0].level.color"
+         [attr.stroke-width]="1"></line>
+   <line [attr.x1]="11" 
+         [attr.x2]="11" 
+         [attr.y1]="0" 
+         [attr.y2]="22"
+         [attr.stroke]="meters[0].level.color"
+         [attr.stroke-width]="1"></line>
+ </svg>
  <svg [attr.width]="width" 
       [attr.height]="height"
       (mousemove)="onMouseMove($event)"
       (mouseleave)="onMouseLeave($event)">
-   <path [attr.d]="path" 
+   <path class="levels"
+         [attr.d]="path" 
          [attr.fill]="color">
    </path>
-   <line [attr.x1]="playhead.points[0].x" 
-         [attr.x2]="playhead.points[1].x" 
-         [attr.y1]="playhead.points[0].y" 
-         [attr.y2]="playhead.points[1].y"
-         [attr.stroke]="playhead.color"></line>
+   <line *ngFor="let meter of meters"
+         [attr.x1]="meter.level.points[0].x" 
+         [attr.x2]="meter.level.points[1].x" 
+         [attr.y1]="meter.playhead.points[0].y" 
+         [attr.y2]="meter.playhead.points[1].y"
+         [attr.stroke]="meter.playhead.color"
+         [attr.stroke-width]="meter.playhead.stroke"
+         (click)="onMeterClick($event, meter)">
+   </line>
+   <line *ngFor="let meter of meters"
+         [attr.x1]="meter.level.points[0].x" 
+         [attr.x2]="meter.level.points[1].x" 
+         [attr.y1]="meter.level.points[0].y" 
+         [attr.y2]="meter.level.points[1].y"
+         [attr.stroke]="meter.level.color"
+         [attr.stroke-width]="meter.level.stroke"
+         (click)="onMeterClick($event, meter)">
+   </line>
  </svg>
 `,
 moduleId: module.id,
@@ -34,8 +65,9 @@ export class Visualizer implements OnInit {
   width: number;
   height: number;
   color: string;
-  playhead: any;
+  meters: any[];
   shape: any;
+  selected: number;
   ref: ChangeDetectorRef;
   //changeDetection: ChangeDetectionStrategy.OnPush;
   
@@ -47,21 +79,11 @@ export class Visualizer implements OnInit {
     this.color = '#8DE969';
     this.width = window.innerWidth;
     this.height = 240;
+    this.meters = []; 
     
-    this.playhead = {
-      color: 'transparent',
-      isVisible: false,
-      points: [{
-        x: 0,
-        y: 0
-      },
-      {
-        x: 0,
-        y: this.height
-      }]
-    };
+    this.addMeter();
 
-  
+   
 
   }
   ngOnInit() {
@@ -70,40 +92,69 @@ export class Visualizer implements OnInit {
             .interpolate('cardinal')
             .x(function(d, i) { return x(i); })
             .y(function(d, i) { return y(d); }),
-        x = d3.scale.linear().domain([0, 1026]).range([0, window.innerWidth]),
+        x = d3.scale.linear().domain([0, 514]).range([0, window.innerWidth]),
         y = d3.scale.linear().domain([0, 255]).range([this.height, 0]);
     
-    this.shape = this.elem.nativeElement.getElementsByTagName('path')[0];
+    this.shape = this.elem.nativeElement.getElementsByClassName('levels')[0];
 
     this.mediaService.emitter.subscribe((res)=>{
       
       res.unshift(0);
       res.push(0);
+
       this.path = line(res);
+
+      
+      for(let i=0; i<this.meters.length i++) {
+        this.meters[i].level.points[0].x = this.meters[i].position.x;
+        this.meters[i].level.points[1].x = this.meters[i].position.x; 
+        this.meters[i].level.points[0].y = this.height - this.shape.getPointAtLength(this.meters[i].position.x).y; 
+        this.meters[i].level.points[1].y = this.height;
+        this.meters[i].val = this.scale(this.height - this.shape.getPointAtLength(this.meters[i].position.x).y, 0, this.height, 0, 255);
+      }
+
+            //console.log(this.meters[0].val);
+      
       this.ref.detectChanges();
 
     });
 
 
+
   }
   onMouseMove(ev) {
-    let x = ev.clientX;
-    let y = 0;
-    //console.log(this.shape.getPointAtLength(ev.clientX));
-    // this.playhead.points[0].x = x;
-    // this.playhead.points[1].x = x; 
-    // this.playhead.points[0].y = this.shape.getPointAtLength(ev.clientX).y;
-    // this.playhead.points[1].y = 0;
-    // this.playhead.isVisible = true; 
-    //this.ref.detectChanges();
+    if(this.selected !== 1000) {
+      this.meters[this.selected].position.x = ev.clientX;
+      this.ref.detectChanges();
+    }
   }
   onMouseLeave(ev) {
     
-    //this.playhead.isVisible = false;
-    console.log( this.playhead.isVisible );
-    //this.ref.detectChanges();
+    // this.playhead.isVisible = false;
+    // this.ref.detectChanges();
 
   }
+  
+  scale(v, min, max, gmin, gmax) {
+    
+      v = parseInt(v);
+      return parseInt(((v - min) / (max - min)) * (gmax - gmin) + gmin);
+   }
+   
+   addMeter(ev) {
+     this.meters.push(new Meter(this.height, this.meters.length));
+     this.selected = this.meters.length - 1;
+   }
+   
+   onMeterClick(ev, meter) {
+     console.log(ev, meter);
+     if(this.selected === meter.index) {
+       this.selected = 1000;
+     } else {
+       this.selected = meter.index;
+     }
+     
+   }
 
 
 
---
+++ app/src/models/meter.ts
@@ -0,0 +1,49 @@
+export class Meter {
+  position: any;
+  level: any;
+  playhead: any;
+  isVisible: boolean;
+  val: number;
+  index: number;
+  constructor(private height: number, private index: number) {
+    
+    this.position = {
+        x: 0,
+        y: 0
+    };
+    
+    this.val = 0;
+    
+    this.isVisible = false;
+    
+    this.index = index;
+    
+    this.level = {
+      color: 'rgba(255,255,255,1.0)',
+      stroke : 6,
+      val: 0,
+      points: [{
+        x: 0,
+        y: height
+      },
+      {
+        x: 0,
+        y: height
+      }]
+    };
+    
+    this.playhead = {
+      color: 'rgba(255,255,255,0.3)',
+      stroke : 6,
+      points: [{
+        x: 0,
+        y: 0
+      },
+      {
+        x: 0,
+        y: height
+      }]
+    };
+  };
+  
+}
